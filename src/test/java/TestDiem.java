2a437ff8942370158064fb4541a5c5d3c86af91e
Quang <hocmai6@gmail.com>
2020-12-26 22:57:36 +0700

    das

---
+++ src/test/java/TestDiem.java
@@ -0,0 +1,26 @@
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class TestDiem {
+
+    private Diem calculation;
+
+    @Before
+    public void init() {
+        calculation = new Diem();
+    }
+
+    @Test
+    public void testAdd() {
+        assertEquals(4, calculation.add(1, 3));
+    }
+
+    @Test
+    public void testSub() {
+        assertEquals(2, calculation.sub(7, 5));
+    }
+
+}
